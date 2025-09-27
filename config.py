d23f862aaec29e3e8a44e404e2008c93ba3308b7
biGGzeta <82134310+biGGzeta@users.noreply.github.com>
2025-09-27 13:05:30 -0300

    Modify trading bot configuration settings
    Updated configuration parameters for trading bot, including API keys, paper mode, grid settings, and fee rates.

--- config.py
+++ config.py
@@ -1,33 +1,32 @@
-API_KEY = 'TU_API_KEY'
-API_SECRET = 'TU_API_SECRET'
+API_KEY = ''
+API_SECRET = ''
+
 
 SYMBOL = 'ETHUSDT'
 LEVERAGE = 10
 
 # Entorno
-PAPER_MODE = True   # Simulación: no llama endpoints privados ni coloca órdenes reales
+PAPER_MODE = False   # Simulación: no llama endpoints privados ni coloca órdenes reales
 USE_TESTNET = False # Para operar en testnet cuando PAPER_MODE=False y tengas claves de testnet
 
 # Grid settings
-GRID_RANGE_MIN = 0.06   # 6%
-GRID_RANGE_MAX = 0.15   # 15%
-MIN_GRID_SPACING = 0.003   # 0.3%
-MAX_GRID_SPACING = 0.0075  # 0.75%
+GRID_RANGE_MIN = 0.003   # 6%
+GRID_RANGE_MAX = 0.0086   # 15%
+MIN_GRID_SPACING = 0.00075   # 0.3%
+MAX_GRID_SPACING = 0.0025  # 0.75%
 ORDER_USDT_SIZE = 10    # Capital por orden (se multiplica por leverage implícitamente)
-REBALANCE_SECONDS = 10
+REBALANCE_SECONDS = 180
 
 # Take profit
 MIN_PROFIT_THRESHOLD = 0.003  # 0.30% target base
 TP_OFFSET_LOW = (0.0003, 0.0003)   # 0.03% y 0.03%
-TP_OFFSET_MID = (0.0005, 0.0005)   # 0.05% y 0.05%
-TP_OFFSET_HIGH = (0.0005, 0.0007)  # 0.05% y 0.07%
+TP_OFFSET_MID = (0.00015, 0.0002)
+TP_OFFSET_HIGH = (0.0002, 0.0003)
 
-# Fees (ajusta según tu cuenta / VIP / BNB)
+STOP_LOSS_PERCENTAGE = 0.05
+
 MAKER_FEE_RATE = 0.0002
-TAKER_FEE_RATE = 0.0004
 
-# SL Global
-STOP_LOSS_PERCENTAGE = 0.15  # 15%
+SAFE_SPREAD = 0.001  # 0.1%: spread mínimo para que la orden de compra realmente mejore el promedio de entrada
 
-# Archivo estado
-STATE_FILE = 'data/bot_state.json'
+STATE_FILE = "state.json"
